9d08276a527d18c12d9e5c8ef4f039c0566d2a57
Mikel Goig <mikel@binalogue.com>
2017-12-22 13:24:22 +0100

    Update migration

--- database/migrations/2017_10_27_132246_add_username_and_avatar_columns_to_users_table.php
+++ database/migrations/2017_10_27_132246_add_username_and_avatar_columns_to_users_table.php
@@ -14,7 +14,7 @@ class AddUsernameAndAvatarColumnsToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('username')->inique();
+            $table->string('username')->unique();
             $table->string('avatar')->nullable();
         });
     }
